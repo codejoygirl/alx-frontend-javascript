8da1e0405fc8681469bebb1f892c476174976249
codejoygirl <joyegbu@gmail.com>
2024-06-24 14:23:51 -0700

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  // Original variables declared with var
   var task = false;
   var task2 = true;
 
-  // Conditionally update variables without redeclaring
   if (trueOrFalse) {
-    task = true;   // Update task without redeclaring
-    task2 = false; // Update task2 without redeclaring
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
