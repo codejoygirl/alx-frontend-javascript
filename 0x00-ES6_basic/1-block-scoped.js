8bbc25fd1d77fbcb2765d3dcefb54eea473542d2
codejoygirl <joyegbu@gmail.com>
2024-06-24 12:58:19 -0700

    second commit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
+    // Using let or const here ensures that task and task2 are not redeclared,
+    // but rather are assigned new values within the existing scope.
     task = true;
     task2 = false;
   }
 
   return [task, task2];
 }
+
