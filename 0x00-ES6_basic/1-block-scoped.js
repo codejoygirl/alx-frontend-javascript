a14e24cc52e3ea8d5c9107198344710e612649e4
codejoygirl <joyegbu@gmail.com>
2024-06-24 13:22:27 -0700

    second commit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  // Use let for block-scoped variables
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    // Declare new variables within the block
     task = true;
     task2 = false;
   }
