35cebfbada4667a6ed38191b9a8eb608b41331fe
codejoygirl <joyegbu@gmail.com>
2024-06-24 16:08:40 -0700

    12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,16 +1,10 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = employeesList.reduce((acc, { department, name }) => {
-    if (!acc[department]) {
-      acc[department] = [];
-    }
-    acc[department].push(name);
-    return acc;
-  }, {});
-
   return {
-    allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(this.allEmployees).length;
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
 }
