99879cc5e0338e3aac73adc59e54fee8b32174fc
Dev.Joy <Joyegbu@gmail.com>
2024-07-02 02:49:34 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,12 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr
-    .filter(student => student.location === city)
-    .map(student => {
-      let grade = newGrades.find(g => g.studentId === student.id);
-      return { ...student, grade: (grade && grade.grade) || 'N/A' };
-    });
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const newStudents = student;
+    newStudents.grade = 'N/A';
+    for (const grade of newGrades) {
+      if (newStudents.id === grade.studentId) {
+        newStudents.grade = grade.grade;
+      }
+    }
+    return newStudents;
+  });
 }
